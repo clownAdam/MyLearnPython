66a34e29ca8b59ac54ab72594124aaaaf8244d7a
clown <3406869297@qq.com>
2020-03-09 23:08:13 +0800

    Python2---分支部分---完

---
+++ 1.py
@@ -0,0 +1,27 @@
+age = 16
+if age >= 18:
+    print("已成年")
+    print("可以上网")
+else:
+    print("未成年人，请回家玩泥巴")
+print("回家")
+
+score = 80
+# if score>=90 and score <=100:
+#     print("优秀")
+# if score>=80 and score<90:
+#     print("良好")
+# if score>=60 and score<80:
+#     print("及格")
+# if score>=0 and score<60:
+#     print("不及格")
+if 90<=score<=100:
+    print("优秀")
+elif 80<=score<90:
+    print("良好")
+elif 60<=score<80:
+    print("及格")
+else:
+     print("不及格")
+str = input("you is me name")
+print(str+"--------------")
